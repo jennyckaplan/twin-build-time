74f48ed6796dfbc6281315b35f09ad11d514aa00
Jen Kaplan <jenniferckaplan@gmail.com>
2022-08-02 00:11:09 -0400

    fix: do not used the styled api

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-import tw, { styled, css } from 'twin.macro'
+import tw, { css } from 'twin.macro'
 
 const baseButtonStyles = tw`
   px-8 py-2 rounded focus:outline-none transform duration-75 hocus:(scale-105 text-yellow-400)
@@ -10,8 +10,8 @@ const containerVariants = {
     tw`bg-black text-white border-black`
   ],
   secondary: [
-    baseButtonStyles, 
-    tw`border-2 border-yellow-600`, 
+    baseButtonStyles,
+    tw`border-2 border-yellow-600`,
     css`
       box-shadow: 0 0.1em 0 0 rgba(0, 0, 0, 0.25);
       color: #728FCE;
@@ -20,5 +20,6 @@ const containerVariants = {
   ],
 }
 
-
-export const Button = styled.button(({ variant }) => containerVariants[variant]);
+export const Button = ({ variant, children, className }) => {
+  return <button css={containerVariants[variant]} className={className}>{children}</button>
+};
